f928a6a08093563debb83ac27e7e89408700ed96
DanielleInkster <danielle.inkster@gmail.com>
2024-05-17 13:46:21 +0100

    changed 100 max to 30

--- app/src/main/kotlin/org/monzo/crawler/entities/CrawlerCLI.kt
+++ app/src/main/kotlin/org/monzo/crawler/entities/CrawlerCLI.kt
@@ -17,9 +17,9 @@ class CrawlerCLI : CliktCommand() {
     val seedUrl: String by option().prompt(
         "Enter seed url",
     ).help("The url string to start crawl.").validate { require(helper.validateUrlString(it) == true) { "See info message." } }
-    val concurrency: Int by option().int().restrictTo(min = 1, max = 100).prompt(
+    val concurrency: Int by option().int().restrictTo(min = 1, max = 30).prompt(
         "Enter concurrency per crawler",
-    ).help("Rate of concurrency per crawler. Between 1 and 100")
+    ).help("Rate of concurrency per crawler. Between 1 and 30")
     val delay: Double by option().double().restrictTo(min = 0.00, max = 200.00).prompt(
         "Set fallback crawl delay in seconds and milliseconds",
     ).help("Rate of delay between page crawls if not set by host. Between 0.00-200.00")
